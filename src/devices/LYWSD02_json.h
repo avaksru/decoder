16c0c8ad55e3f19a18a2d6e2494d13209d987b02
avaksru <33891999+avaksru@users.noreply.github.com>
2022-04-04 10:27:51 +0300

    krasnikov

--- src/devices/LYWSD02_json.h
+++ src/devices/LYWSD02_json.h
@@ -1,12 +1,14 @@
 #include "common_props.h"
 
-const char* _LYWSD02_json = "{\"brand\":\"Xiaomi\",\"model\":\"Cleargrass clock\",\"model_id\":\"LYWSD02\",\"condition\":[\"servicedata\",\"index\",2,\"205b04\"],\"properties\":{\"tempc\":{\"condition\":[\"servicedata\",25,\"4\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"condition\":[\"servicedata\",25,\"6\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,4,true,false],\"post_proc\":[\"/\",10]}}}";
+//const char* _LYWSD02_json = "{\"brand\":\"Xiaomi\",\"model\":\"Cleargrass clock\",\"model_id\":\"LYWSD02\",\"condition\":[\"servicedata\",\"contain\",\"205b04\"],\"properties\":{\"tempc\":{\"condition\":[\"servicedata\",25,\"4\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"condition\":[\"servicedata\",25,\"6\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,4,true,false],\"post_proc\":[\"/\",10]}}}";
+  const char* _LYWSD02_json = "{\"brand\":\"Xiaomi\",\"model\":\"Cleargrass clock\",\"model_id\":\"LYWSD02\",\"condition\":[\"servicedata\",\"contain\",\"205b04\"],\"properties\":{\"tempc\":{\"condition\":[\"servicedata\",25,\"4\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,4,true],\"post_proc\":[\"/\",10]},\"hum\":{\"condition\":[\"servicedata\",25,\"6\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,4,true],\"post_proc\":[\"/\",10]},\"batt\":{\"condition\":[\"servicedata\",25,\"a\"],\"decoder\":[\"value_from_hex_data\",\"servicedata\",30,2,true,false]}}}";
+
 /* R""""(
 {
    "brand":"Xiaomi",
    "model":"Cleargrass clock",
    "model_id":"LYWSD02",
-   "condition":[""servicedata", "index", 2, "205b04"],
+   "condition":["servicedata", "contain", "205b04"],
    "properties":{
       "tempc":{
          "condition":["servicedata", 25, "4"],
@@ -15,10 +17,17 @@ const char* _LYWSD02_json = "{\"brand\":\"Xiaomi\",\"model\":\"Cleargrass clock\
       },
       "hum":{
          "condition":["servicedata", 25, "6"],
-         "decoder":["value_from_hex_data", "servicedata", 30, 4, true, false],
+         "decoder":["value_from_hex_data", "servicedata", 30, 4, true],
          "post_proc":["/", 10]
+      },
+      "batt":{
+         "condition":["servicedata", 25, "A"],
+         "decoder":["value_from_hex_data", "servicedata", 30, 2, true, false]   
       }
+
+
    }
 })"""";*/
 
 const char* _LYWSD02_json_props = _common_BTH_props;
+ 
